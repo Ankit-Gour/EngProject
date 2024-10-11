90b324b60ef611004ca75fce5a40029fd76088c5
Ankit Gour <127835812+Ankit-Gour@users.noreply.github.com>
2024-10-11 11:22:56 +0530

    loader added

--- js/custom.js
+++ js/custom.js
@@ -20,3 +20,14 @@ function getCurrentYear() {
 getCurrentYear();
 
 
+window.onload = function() {
+    // Hide the loader after a brief period (simulate loading time)
+    setTimeout(() => {
+        const loader = document.getElementById("loader");
+        const content = document.getElementById("content");
+        
+        // Remove the loader from the DOM
+        loader.style.display = "none";
+        content.style.display = "flex"; // Show the content
+    }, 200); // Change the time (in milliseconds) as needed
+};
